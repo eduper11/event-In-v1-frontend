f0ca373280a57fef19664a19aa752e43d9de43dd
Edu Perez <eduper11@gmail.com>
2019-06-10 01:00:04 +0200

    home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { GeneralLayoutComponent } from './layout/general-layout/general-layout.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 const routes: Routes = [
   {
@@ -32,6 +33,33 @@ const routes: Routes = [
       {
         path: 'start',
         loadChildren: './features/start/start.module#StartModule'
+      }
+    ]
+  },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'homepage',
+        loadChildren: './features/home/home.module#HomeModule'
+      },
+      {
+        path: 'events',
+        loadChildren: './features/events/events.module#EventsModule'
+      },
+      {
+        path: 'live',
+        loadChildren: './features/live/live.module#LiveModule'
+      },
+      {
+        path: 'members',
+        loadChildren: './features/members/members.module#MembersModule'
+      },
+      {
+        path: 'profile',
+        loadChildren: './features/profile/profile.module#ProfileModule'
       }
     ]
   },
