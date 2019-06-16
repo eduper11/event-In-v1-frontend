9a827986b7173f8eaff10354d70154d0d643e9ab
Edu Perez <eduper11@gmail.com>
2019-06-16 20:44:04 +0200

    home feature

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -9,11 +9,36 @@ import { tap } from 'rxjs/operators';
 })
 export class UserService {
   currentUser: User;
+  currentEventUsers: User[];
+
   constructor(private http: HttpClient) {}
+
+  //getUsers pide los users que pertenecen a ese evento en concreto
+
+  getUsers(id) {
+    return this.http
+      .get(`${environment.apiBaseUrl}/event/users`, {
+        params: {
+          event_id: id
+        }
+      })
+      .pipe(tap((users: User[]) => (this.currentEventUsers = users)));
+  }
 
   getUserProfile() {
     return this.http
       .get(`${environment.apiBaseUrl}/user`)
       .pipe(tap((user: User) => (this.currentUser = user)));
+  }
+
+  updateProfile(profile) {
+    return this.http.put(`${environment.apiBaseUrl}/user`, profile).pipe(
+      tap(() => {
+        this.currentUser = {
+          ...this.currentUser,
+          ...profile
+        };
+      })
+    );
   }
 }
