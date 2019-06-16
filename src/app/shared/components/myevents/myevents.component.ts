413ff593694825f03a85f97376242fa6f3647719
Edu Perez <eduper11@gmail.com>
2019-06-16 20:44:37 +0200

    my events component

---
+++ src/app/shared/components/myevents/myevents.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { EventsService } from 'src/app/core/services/events.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'sn-myevents',
+  templateUrl: './myevents.component.html',
+  styleUrls: ['./myevents.component.scss']
+})
+export class MyEventsComponent implements OnInit {
+  constructor(private router: Router, public eventsService: EventsService) {}
+
+  ngOnInit() {
+    this.eventsService.getMyEvents().subscribe();
+  }
+
+  getCurrentEvent(id: string) {
+    this.eventsService.getCurrentEvent(id).subscribe(() => {
+      console.log(this.eventsService);
+      this.router.navigate(['/homepage', id]);
+    });
+  }
+}
