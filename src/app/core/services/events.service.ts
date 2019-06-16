459152490739874c7531e4950510659cbdd8c936
Edu Perez <eduper11@gmail.com>
2019-06-16 20:43:22 +0200

    events service name change

---
+++ src/app/core/services/events.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Router, ActivationEnd } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { Event } from 'src/app/core/core.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+  events: Event[];
+  currentEvent: Event | {} = {};
+  eventsNotJoined: Event[];
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.router.events.subscribe(event => {
+      console.log(event);
+      if (event instanceof ActivationEnd) {
+        if (event.snapshot && event.snapshot.params.id) {
+          this.currentEvent['id'] = event.snapshot.params.id;
+        }
+      }
+    });
+  }
+
+  joinEvent(id: string, rol: string) {
+    return this.http
+      .post(`${environment.apiBaseUrl}/event/join`, {
+        params: {
+          event_id: id,
+          rol: rol
+        }
+      })
+      .pipe();
+  }
+
+  getEventsNotJoined() {
+    return this.http.get(`${environment.apiBaseUrl}/events`).pipe(
+      tap((eventsNotJoined: Event[]) => {
+        this.eventsNotJoined = eventsNotJoined;
+      })
+    );
+  }
+
+  getMyEvents() {
+    return this.http.get(`${environment.apiBaseUrl}/myevents`).pipe(
+      tap((events: Event[]) => {
+        this.events = events;
+      })
+    );
+  }
+
+  getCurrentEvent(id: string) {
+    return this.http
+      .get(`${environment.apiBaseUrl}/eventbyid`, {
+        params: {
+          event_id: id
+        }
+      })
+      .pipe(
+        tap((event: Event) => {
+          this.currentEvent = event[0];
+          console.log(this.currentEvent);
+        })
+      );
+  }
+}
