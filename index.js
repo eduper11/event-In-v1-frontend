1f030d44708bbb03047860a076df60bf4631b8de
Edu Perez <eduper11@gmail.com>
2019-06-18 17:36:44 +0200

    ...

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/event-In'));
+app.use(express.static(__dirname + '/dist/event-in'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/event-In/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/event-in/index.html'));
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 3000);
