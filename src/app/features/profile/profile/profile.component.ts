36ded8b5a82670f70e72ec4533e8c7282af6770c
Edu Perez <eduper11@gmail.com>
2019-06-21 12:20:03 +0200

    linkedin fix bug

--- src/app/features/profile/profile/profile.component.ts
+++ src/app/features/profile/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { UserService } from 'src/app/core/services/user.service';
+import { ModalService } from 'src/app/core/services/modal.service';
 
 @Component({
   selector: 'sn-profile',
@@ -9,29 +10,41 @@ import { UserService } from 'src/app/core/services/user.service';
 })
 export class ProfileComponent implements OnInit {
   updateProfileForm = this.fb.group({
-    fullName: ['', [Validators.required]],
-    linkedIn: ['', [Validators.nullValidator]],
+    full_name: ['', [Validators.required]],
+    linkedin: ['', [Validators.nullValidator]],
     twitter: ['', [Validators.nullValidator]],
     github: ['', [Validators.nullValidator]],
     description: ['', [Validators.nullValidator]]
   });
 
-  constructor(private fb: FormBuilder, private userService: UserService) {}
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private modalService: ModalService
+  ) {}
 
   ngOnInit() {
-    // this.userService.getUserProfile().subscribe();
-    this.updateProfileForm.setValue({
-      fullName: this.userService.currentUser[0].full_name,
-      linkedIn: this.userService.currentUser[0].linkedin,
-      twitter: this.userService.currentUser[0].twitter,
-      github: this.userService.currentUser[0].github,
-      description: this.userService.currentUser[0].description
+    this.userService.getUserProfile().subscribe(data => {
+      this.updateProfileForm.setValue({
+        full_name: this.userService.currentUser[0].full_name,
+        linkedin: this.userService.currentUser[0].linkedin,
+        twitter: this.userService.currentUser[0].twitter,
+        github: this.userService.currentUser[0].github,
+        description: this.userService.currentUser[0].description
+      });
     });
   }
 
   updateProfile() {
     if (this.updateProfileForm.valid) {
-      this.userService.updateProfile(this.updateProfileForm.value).subscribe();
+      this.userService
+        .updateProfile(this.updateProfileForm.value)
+        .subscribe(() => {
+          this.modalService.open(
+            'Profile updated!!',
+            'Now your event mates can learn more about you'
+          );
+        });
     }
   }
 }
